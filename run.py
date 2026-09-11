efde816a7371c5cf80722a0030b1990c02e35afd
jonah855 <jonah@bergkvists.com>
2026-09-11 23:01:10 +0200

    Wire invoice PDF hardening into launcher

--- run.py
+++ run.py
@@ -2,10 +2,12 @@ import threading
 import app
 from features import register_features
 from invoice_upgrade import register_invoice_upgrade
+from invoice_hardening import register_invoice_hardening
 from nav_upgrade import register_navigation_upgrade
 
 register_features(app.app, app.conn, app.setting, app.set_setting, app.create_voucher, app.money, app.AC, app.ROOT, app.EXPORT)
 register_invoice_upgrade(app.app, app.conn, app.setting, app.set_setting, app.create_voucher, app.money, app.AC, app.ROOT, app.EXPORT)
+register_invoice_hardening(app.app, app.conn, app.setting, app.EXPORT)
 register_navigation_upgrade()
 
 if __name__ == '__main__':
