5db9a5cb8d066e132638b72912e7125bf259a581
jonah855 <jonah@bergkvists.com>
2026-09-11 23:04:51 +0200

    Add invoice payments to main navigation

--- nav_upgrade.py
+++ nav_upgrade.py
@@ -2,8 +2,8 @@ import app as core
 
 
 def register_navigation_upgrade():
-    """Add invoice navigation to the shared v13 navigation without replacing the core app."""
+    """Add the business modules to the shared v13 navigation without replacing the core app."""
     nav_old = '<a href="/receipts">Kvitton</a>'
-    nav_new = nav_old + '<a href="/invoices">Fakturor</a><a href="/customers">Kunder</a><a href="/articles">Artiklar</a>'
+    nav_new = nav_old + '<a href="/invoices">Fakturor</a><a href="/invoice-payments">Betalningar</a><a href="/customers">Kunder</a><a href="/articles">Artiklar</a>'
     if nav_old in core.HTML and 'href="/invoices"' not in core.HTML:
         core.HTML = core.HTML.replace(nav_old, nav_new)
