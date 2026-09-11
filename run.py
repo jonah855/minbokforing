32b60248efbb74574a02644bdea6d35dc9777844
jonah855 <jonah@bergkvists.com>
2026-09-11 22:51:09 +0200

    Enable invoice navigation in desktop app

--- run.py
+++ run.py
@@ -2,9 +2,11 @@ import threading
 import app
 from features import register_features
 from invoice_upgrade import register_invoice_upgrade
+from nav_upgrade import register_navigation_upgrade
 
 register_features(app.app, app.conn, app.setting, app.set_setting, app.create_voucher, app.money, app.AC, app.ROOT, app.EXPORT)
 register_invoice_upgrade(app.app, app.conn, app.setting, app.set_setting, app.create_voucher, app.money, app.AC, app.ROOT, app.EXPORT)
+register_navigation_upgrade()
 
 if __name__ == '__main__':
     app.conn()
